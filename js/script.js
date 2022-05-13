5c3c38e223f6308a145fd261cc482c9b5a5f0479
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-13 15:07:32 +0200

    aggiunta visualizzazione di 10 email

--- js/script.js
+++ js/script.js
@@ -9,16 +9,21 @@ const app = new Vue (
     {
         el : '#root',
         data: {
-            randomMail: ''           
+            randomMail: '',
+            eMailList : []           
         },
 
         methods: {
+            
             generateMail() {
-                axios
-                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-                    .then((resp) => {
-                        this.randomMail = resp.data.response;
-                    });
+                for (let i = 0; i< 10; i++) {
+                    axios
+                        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                        .then((resp) => {
+                            this.randomMail = resp.data.response;
+                            this.eMailList.push(this.randomMail);
+                        });
+                }
             }
             
         }
